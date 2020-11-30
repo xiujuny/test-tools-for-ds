45c71588e21e67a30c21e253b3c07a9171fc267a
filteryang <68161640+filteryang@users.noreply.github.com>
2020-11-30 10:31:51 -0600

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+print('Hello world!')
+# name = input ('what is your name?')
+print('Hello')
+# print(name)
+print('is poppy')
+x = 625 + 78
+print(x)
+print('I did math!')
+# this line does nothing
