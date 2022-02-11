1c85f250fda566f8def3bec89ce3fa7c050df01c
Jenny Yang <68161640+filteryang@users.noreply.github.com>
2022-02-11 08:49:02 -0600

    Delete hello_world.py

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-print('Hello world!')
-# name = input ('what is your name?')
-print('Hello')
-# print(name)
-print('is poppy')
-x = 625 + 78
-print(x)
-print('I did math!')
-# this line does nothing
